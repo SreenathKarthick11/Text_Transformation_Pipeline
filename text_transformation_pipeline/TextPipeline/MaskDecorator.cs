5440555b1f4eb898691afe397cb61858395ee3ce
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 21:00:12 +0530

    TTP : added MaskDecorator.

---
+++ text_transformation_pipeline/TextPipeline/MaskDecorator.cs
@@ -0,0 +1,22 @@
+﻿
+// The MaskDecorator class.
+// It inherits from the TextDecorator class and overrides the Process method to mask all letters and digits in the input string with asterisks.
+
+namespace TextPipeline;
+public class MaskDecorator(ITextProcessor inner) : TextDecorator(inner)
+{
+    public override string Process(string input)
+    {
+        string processed = Inner.Process(input);
+
+        for (int i = 0; i < processed.Length; i++)
+        {
+            if (char.IsLetterOrDigit(processed[i]))
+            {
+                processed = processed.Remove(i, 1).Insert(i, "*");
+            }
+        }
+
+        return processed;
+    }
+}
