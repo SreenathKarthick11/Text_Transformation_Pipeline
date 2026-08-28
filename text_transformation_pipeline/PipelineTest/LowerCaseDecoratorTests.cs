be4f316a57cf86615af98ea4aa94bbab9b6298d7
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 21:54:10 +0530

    Test : Added LowerCase decorator test.

---
+++ text_transformation_pipeline/PipelineTest/LowerCaseDecoratorTests.cs
@@ -0,0 +1,68 @@
+﻿using TextPipeline;
+
+namespace PipelineTest;
+
+[TestClass]
+public sealed class LowerCaseDecoratorTests
+{
+    [TestMethod]
+    public void LowerCase_UppercaseText()
+    {
+        ITextProcessor processor = new LowerCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("HELLO WORLD");
+
+        Assert.AreEqual("hello world", result);
+    }
+
+    [TestMethod]
+    public void LowerCase_MixedCaseText()
+    {
+        ITextProcessor processor = new LowerCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("HeLLo WoRLd");
+
+        Assert.AreEqual("hello world", result);
+    }
+
+    [TestMethod]
+    public void LowerCase_LowercaseTextUnchanged()
+    {
+        ITextProcessor processor = new LowerCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("hello world");
+
+        Assert.AreEqual("hello world", result);
+    }
+
+    [TestMethod]
+    public void LowerCase_NumbersAndSymbols()
+    {
+        ITextProcessor processor = new LowerCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("HELLO 123! @#$");
+
+        Assert.AreEqual("hello 123! @#$", result);
+    }
+
+    [TestMethod]
+    public void LowerCase_EmptyString()
+    {
+        ITextProcessor processor = new LowerCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("");
+
+        Assert.AreEqual("", result);
+    }
+
+    [TestMethod]
+    public void LowerCase_PreservesWhitespace()
+    {
+        ITextProcessor processor = new LowerCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("   HELLO WORLD   ");
+
+        Assert.AreEqual("   hello world   ", result);
+    }
+
+}
