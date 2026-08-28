3e65b777f1673282b61ee1812a9a47a0d4ef964b
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 20:59:09 +0530

    TTP : added TrimDecorator.

---
+++ text_transformation_pipeline/TextPipeline/TrimDecorator.cs
@@ -0,0 +1,12 @@
+﻿
+// The TrimDecorator class.
+// It inherits from the TextDecorator class and overrides the Process method to trim whitespace from the input string.
+
+namespace TextPipeline;
+public class TrimDecorator(ITextProcessor inner) : TextDecorator(inner)
+{
+    public override string Process(string input)
+    {
+        return Inner.Process(input).Trim();
+    }
+}
