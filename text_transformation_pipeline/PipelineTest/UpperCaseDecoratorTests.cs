9d5da5cf9fe50c2230f9ed64fcf89648765917df
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 21:50:49 +0530

    Test : renamed file name, due to typo.

---
+++ text_transformation_pipeline/PipelineTest/UpperCaseDecoratorTests.cs
@@ -0,0 +1,67 @@
+﻿using TextPipeline;
+
+namespace PipelineTest;
+
+[TestClass]
+public sealed class UpperCaseDecoratorTests
+{
+    [TestMethod]
+    public void UpperCase_LowercaseText()
+    {
+        ITextProcessor processor = new UpperCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("hello world");
+
+        Assert.AreEqual("HELLO WORLD", result);
+    }
+
+    [TestMethod]
+    public void UpperCase_MixedCaseText()
+    {
+        ITextProcessor processor = new UpperCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("HeLLo WoRLd");
+
+        Assert.AreEqual("HELLO WORLD", result);
+    }
+
+    [TestMethod]
+    public void UpperCase_UppercaseTextUnchanged()
+    {
+        ITextProcessor processor = new UpperCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("HELLO WORLD");
+
+        Assert.AreEqual("HELLO WORLD", result);
+    }
+
+    [TestMethod]
+    public void UpperCase_NumbersAndSymbols()
+    {
+        ITextProcessor processor = new UpperCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("hello 123! @#$");
+
+        Assert.AreEqual("HELLO 123! @#$", result);
+    }
+
+    [TestMethod]
+    public void UpperCase_EmptyString()
+    {
+        ITextProcessor processor = new UpperCaseDecorator(new TextProcessor());
+
+        string result = processor.Process("");
+
+        Assert.AreEqual("", result);
+    }
+
+    [TestMethod]
+    public void UpperCase_PreservesWhitespace()
+    {
+        ITextProcessor processor = new UpperCaseDecorator(new TextProcessor());
+        string result = processor.Process("   hello world   ");
+
+        Assert.AreEqual("   HELLO WORLD   ", result);
+    }
+
+}
