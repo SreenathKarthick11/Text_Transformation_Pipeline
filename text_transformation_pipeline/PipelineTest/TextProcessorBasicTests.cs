14c056d932f43c37ad4ebb47a17f25854adfe12c
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 21:25:48 +0530

    Test : Added basic text processor test.

---
+++ text_transformation_pipeline/PipelineTest/TextProcessorBasicTests.cs
@@ -0,0 +1,47 @@
+﻿using TextPipeline;
+
+namespace PipelineTest;
+
+[TestClass]
+public sealed class TextProcessorBasicTests
+{
+    [TestMethod]
+    public void Process_ReturnsInputUnchanged()
+    {
+        ITextProcessor processor = new TextProcessor();
+
+        string result = processor.Process("Hello World");
+
+        Assert.AreEqual("Hello World", result);
+    }
+
+    [TestMethod]
+    public void Process_PreservesLeadingAndTrailingWhitespace()
+    {
+        ITextProcessor processor = new TextProcessor();
+
+        string result = processor.Process("   Hello World   ");
+
+        Assert.AreEqual("   Hello World   ", result);
+    }
+
+    [TestMethod]
+    public void Process_HandlesEmptyString()
+    {
+        ITextProcessor processor = new TextProcessor();
+
+        string result = processor.Process("");
+
+        Assert.AreEqual("", result);
+    }
+
+    [TestMethod]
+    public void Process_PreservesSpecialCharacters()
+    {
+        ITextProcessor processor = new TextProcessor();
+
+        string result = processor.Process("Hello, World! 123.");
+
+        Assert.AreEqual("Hello, World! 123.", result);
+    }
+}
