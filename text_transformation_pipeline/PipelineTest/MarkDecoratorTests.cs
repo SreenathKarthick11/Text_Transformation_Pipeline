801bc0a150c82be8c5f851cc164e04647007ec00
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 21:40:05 +0530

    Test : Added Mask decorator test.

---
+++ text_transformation_pipeline/PipelineTest/MarkDecoratorTests.cs
@@ -0,0 +1,67 @@
+﻿using TextPipeline;
+
+namespace PipelineTest;
+
+[TestClass]
+public sealed class MaskDecoratorTests
+{
+    [TestMethod]
+    public void Mask_ReplacesCharactersWithAsterisks()
+    {
+        ITextProcessor processor = new MaskDecorator(new TextProcessor());
+
+        string result = processor.Process("Hello");
+
+        Assert.AreEqual("*****", result);
+    }
+
+    [TestMethod]
+    public void Mask_PreservesSpaces()
+    {
+        ITextProcessor processor = new MaskDecorator(new TextProcessor());
+
+        string result = processor.Process("Hello  World");
+
+        Assert.AreEqual("*****  *****", result);
+    }
+
+
+    [TestMethod]
+    public void Mask_EmptyString()
+    {
+        ITextProcessor processor = new MaskDecorator(new TextProcessor());
+        string result = processor.Process("");
+
+        Assert.AreEqual("", result);
+    }
+
+    [TestMethod]
+    public void Mask_OnlyWhitespace()
+    {
+        ITextProcessor processor = new MaskDecorator(new TextProcessor());
+
+        string result = processor.Process("     ");
+
+        Assert.AreEqual("     ", result);
+    }
+
+    [TestMethod]
+    public void Mask_NumbersAndSymbols()
+    {
+        ITextProcessor processor = new MaskDecorator(new TextProcessor());
+
+        string result = processor.Process("ABC123!@#");
+
+        Assert.AreEqual("*********", result);
+    }
+
+    [TestMethod]
+    public void Mask_NewlinesAndTabs()
+    {
+        ITextProcessor processor = new MaskDecorator(new TextProcessor());
+
+        string result = processor.Process("Hello\nWorld\tTest");
+
+        Assert.AreEqual("*****\n*****\t****", result);
+    }
+}
