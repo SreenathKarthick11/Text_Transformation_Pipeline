53bf3290217be40febd8e6c246947e99e55cf652
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-08-28 21:02:09 +0530

    TTP : added UpperCase and LowerCase Decorator.

---
+++ text_transformation_pipeline/TextPipeline/LowerCaseDecorator.cs
@@ -0,0 +1,12 @@
+﻿
+// The LowerCaseDecorator class.
+// It inherits from the TextDecorator class and overrides the Process method to convert the input string to lowercase.
+
+namespace TextPipeline;
+public class LowerCaseDecorator(ITextProcessor inner) : TextDecorator(inner)
+{
+    public override string Process(string input)
+    {
+        return Inner.Process(input).ToLowerInvariant();
+    }
+}
---
+++ text_transformation_pipeline/TextPipeline/UpperCaseDecorator.cs
@@ -0,0 +1,13 @@
+﻿
+// The UpperCaseDecorator class.
+// It inherits from the TextDecorator class and overrides the Process method to convert the input string to uppercase.
+
+namespace TextPipeline;
+
+public class UpperCaseDecorator(ITextProcessor inner) : TextDecorator(inner)
+{
+    public override string Process(string input)
+    {
+        return Inner.Process(input).ToUpperInvariant();
+    }
+}
